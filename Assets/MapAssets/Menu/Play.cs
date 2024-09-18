067727eb16118e5dc76b900ba37286e4241b3fb2
LâmLmao <thanhlam03082004@gmail.com>
2024-09-18 20:07:29 +0700

    Initial commit

---
+++ Assets/MapAssets/Menu/Play.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Play : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    public void ChoiMoi()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Thoat()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    public float speed = 8f;
+    public float jumpingPower = 16f;
+    private bool isFacingRight = true;
+    public Animator animator;
+    private int jumpcount = 0;
+    public int maxhealth = 100;
+    public int currenthealth;
+    public HealthBar healthBar;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    void Start()
+    {
+        currenthealth = maxhealth;
+        healthBar.SetMaxHealth(maxhealth);
+        Application.targetFrameRate = 60;
+
+    }
+
+
+    void Update()
+    {
+        
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+           if(jumpcount < 1)
+            {
+                jumpcount++;
+            }
+        }
+        else if (Input.GetButtonDown("Jump") && jumpcount < 1 && IsGrounded()==false)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            jumpcount++;
+        }
+        animator.SetBool("IsGround", IsGrounded());
+        Flip();
+        if(IsGrounded())
+        {
+            jumpcount = 0;
+        }
+        animator.SetInteger("Speed", (int)Mathf.Abs(horizontal));
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            animator.SetBool("IsUsingSword", false);
+        }
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Sword") && Input.GetKey(KeyCode.E))
+        {
+            animator.SetBool("IsUsingSword",true);
+            Destroy(collision.gameObject);
+        }
+    }
+} 
