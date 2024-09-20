cc8ab8621c1d97d6a765ac8889d9a839a52ade1a
Lzio1310 <128127765+Lzio1310@users.noreply.github.com>
2024-09-20 15:56:41 +0700

    9/20quang

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -61,10 +61,12 @@ public class PlayerMovement : MonoBehaviour
        
 
     }
+ 
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
     }
 
     private bool IsGrounded()
