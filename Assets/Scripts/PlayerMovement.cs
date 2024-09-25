cd7796cb07ae9211b1e41e709d1328aadba1c3b8
LâmLmao <thanhlam03082004@gmail.com>
2024-09-25 21:37:04 +0700

    setup lai file

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,19 +12,7 @@ public class PlayerMovement : MonoBehaviour
     private int jumpcount = 0;
     public int maxhealth = 100;
     public int currenthealth;
-    public int damage = 20;
-    private bool isAttacking = false;
-    public float attackRange = 0.5f;
-    public LayerMask enemyLayers;
-    public Transform attackPoint;
-    private float attackCooldownTimer = 0f;
-    public float attackCooldownDuration = 1f;
 
-    private RangeWeapon rangeWeapon;
-    public float bowcooldown = 1f;
-
-    //yes testing//
-    // public HealthBar healthBar;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -34,109 +22,38 @@ public class PlayerMovement : MonoBehaviour
     {
         currenthealth = maxhealth;
         Application.targetFrameRate = 60;
-        if (rangeWeapon == null)
-        {
-            rangeWeapon = GetComponent<RangeWeapon>();
-        }
-
     }
 
 
     void Update()
     {
-        if (attackCooldownTimer > 0)
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            attackCooldownTimer -= Time.deltaTime;
-        }
-        if (bowcooldown > 0)
-        {
-            bowcooldown -= Time.deltaTime;
-        }
-        if (Input.GetMouseButtonDown(0) && attackCooldownTimer <= 0)
-        {
-            Attack();
-        }
-
-        if (Input.GetMouseButtonDown(1) && bowcooldown <= 0)
-        {
-            Bow();
-        }
-        if (!isAttacking)
-        {
-            horizontal = Input.GetAxisRaw("Horizontal");
-            if (Input.GetButtonDown("Jump") && IsGrounded())
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            if (jumpcount < 1)
             {
-                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-                if (jumpcount < 1)
-                {
-                    jumpcount++;
-                }
-            }
-            else if (Input.GetButtonDown("Jump") && jumpcount < 1 && IsGrounded() == false)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
                 jumpcount++;
             }
-            animator.SetBool("IsGround", IsGrounded());
-            Flip();
-            if (IsGrounded())
-            {
-                jumpcount = 0;
-            }
-            animator.SetInteger("Speed", (int)Mathf.Abs(horizontal));
         }
-    }
-
-    private void EndAttack()
-    {
-        isAttacking = false;
+        else if (Input.GetButtonDown("Jump") && jumpcount < 1 && IsGrounded() == false)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            jumpcount++;
+        }
+        animator.SetBool("IsGround", IsGrounded());
+        Flip();
+        if (IsGrounded())
+        {
+            jumpcount = 0;
+        }
+        animator.SetInteger("Speed", (int)Mathf.Abs(horizontal));
     }
 
     private void FixedUpdate()
     {
-        if (!isAttacking)
-        {
-            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        }
-        else
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        }
-
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
-
-
-    private void Attack()
-    {
-        animator.SetTrigger("Attack");
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-        foreach (Collider2D enemy in hitEnemies)
-        {
-            // enemy.GetComponent<BoarScript>().TakeDamage(damage);    
-            Debug.Log("We hit " + enemy.name);
-        }
-        attackCooldownTimer = attackCooldownDuration;
-    }
-
-    private void Bow()
-    {
-        animator.SetTrigger("Bow");
-        isAttacking = true;
-        rangeWeapon.Shoot();
-        Invoke("EndAttack", 1f);
-        attackCooldownTimer = bowcooldown;
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if (attackPoint == null)
-        {
-            return;
-        }
-
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-    }
-
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
@@ -152,4 +69,5 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = localScale;
         }
     }
+
 }
