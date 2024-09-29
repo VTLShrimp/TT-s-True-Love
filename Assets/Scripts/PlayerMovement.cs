081706de04e694b32226285597240bf358e4c1ef
Lzio1310 <128127765+Lzio1310@users.noreply.github.com>
2024-09-29 14:55:20 +0700

    Revert "Merge branch 'LamLmao2' into quang2"
    This reverts commit 6d43531b67f9f463030de5f65216b1d9a2db8388, reversing
    changes made to 833282f5de5fdd9d16e873d6d5ddab77faa2bd7a.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using UnityEngine;
-
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,7 +10,16 @@ public class PlayerMovement : MonoBehaviour
     private int jumpcount = 0;
     public int maxhealth = 100;
     public int currenthealth;
+    public int damage = 20;
+    private bool isAttacking = false;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public Transform attackPoint;
+    private float attackCooldownTimer = 0f;
+    public float attackCooldownDuration = 1f;
 
+    //yes testing//
+    // public HealthBar healthBar;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -21,39 +28,96 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         currenthealth = maxhealth;
+        
         Application.targetFrameRate = 60;
+
     }
 
 
     void Update()
     {
+         if (attackCooldownTimer > 0)
+        {
+            attackCooldownTimer -= Time.deltaTime;
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            Bow();
+        }
+        if(!isAttacking){
         horizontal = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            if (jumpcount < 1)
+           if(jumpcount < 1)
             {
                 jumpcount++;
             }
         }
-        else if (Input.GetButtonDown("Jump") && jumpcount < 1 && IsGrounded() == false)
+        else if (Input.GetButtonDown("Jump") && jumpcount < 1 && IsGrounded()==false)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
             jumpcount++;
         }
         animator.SetBool("IsGround", IsGrounded());
         Flip();
-        if (IsGrounded())
+        if(IsGrounded())
         {
             jumpcount = 0;
         }
         animator.SetInteger("Speed", (int)Mathf.Abs(horizontal));
+        }
+    }
+ 
+private void EndAttack()
+    {
+        isAttacking = false;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+          if (!isAttacking)
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
     }
+
+
+    private void Attack(){
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach(Collider2D enemy in hitEnemies){
+                // enemy.GetComponent<BoarScript>().TakeDamage(damage);    
+                Debug.Log("We hit " + enemy.name);   
+        }
+        attackCooldownTimer = attackCooldownDuration;
+    }
+
+    private void Bow(){
+        animator.SetTrigger("Bow");
+        isAttacking = true;
+        Invoke("EndAttack", 1f);
+    }
+
+
+    void OnDrawGizmosSelected(){
+        if(attackPoint == null){
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
@@ -69,5 +133,12 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = localScale;
         }
     }
-
-}
+    // private void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Sword") && Input.GetKey(KeyCode.E))
+    //     {
+    //         animator.SetBool("IsUsingSword",true);
+    //         Destroy(collision.gameObject);
+    //     }
+    // }
+} 
