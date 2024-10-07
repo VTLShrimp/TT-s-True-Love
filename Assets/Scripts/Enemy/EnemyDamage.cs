285633b2f6ba6e5632e1b9e8a0c9ddd99a5dd3f2
LâmLmao <thanhlam03082004@gmail.com>
2024-10-07 10:16:47 +0700

    update nhe

--- Assets/Scripts/Enemy/EnemyDamage.cs
+++ Assets/Scripts/Enemy/EnemyDamage.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 
+
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] protected float damage;
 
     protected void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
             collision.GetComponent<PlayerHealth>().TakeDamage(damage);
     }
 }
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,37 +8,22 @@ public class PlayerHealth : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
-        // Không cần cập nhật UI nữa
+
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
         Debug.Log("Player took damage! Current health: " + currentHealth);
-
-        // Không cần cập nhật UI nữa
-
         if (currentHealth <= 0)
         {
             Die();
         }
     }
 
-    // Hàm xử lý khi nhân vật chết
+
     private void Die()
     {
         Debug.Log("Player died!");
-        // Thoát game sau khi nhân vật chết
-        QuitGame();
-    }
-
-    // Hàm thoát game
-    void QuitGame()
-    {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
     }
 }
