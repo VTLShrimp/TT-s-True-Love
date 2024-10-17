ff7e2eed39765c4f3d61c49ba2a38c8aa504e2c0
Lzio1310 <128127765+Lzio1310@users.noreply.github.com>
2024-10-17 16:38:11 +0700

    Revert "sai tùm lum"
    This reverts commit d51937867d2b669a35d833efbe93a27d4fbd69d0.

---
+++ Assets/Scripts/Boss/Boss_Walk.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Boss_Walk : StateMachineBehaviour
+{
+    public float attackRange = 5f; // Range within which the boss will attack the player
+    public float speed = 10f;
+    Transform player;
+    Rigidbody2D rb;
+    Boss boss;
+
+    // OnStateEnter is called when the state starts
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindWithTag("Player").transform; // Find the player object
+        rb = animator.GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component of the boss
+        boss = animator.GetComponent<Boss>(); // Get the Boss script
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (boss.playerInzone)
+        {
+            boss.LookatPlayer(); // Boss looks at the player
+
+            // Move the boss towards the player
+            Vector2 target = new(player.position.x, rb.position.y);
+            Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+            rb.transform.position = newPos;
+
+            // Check if the boss is within attack range
+            if (Vector2.Distance(player.position, rb.position) <= attackRange)
+            {
+                animator.SetTrigger("Attack"); // Trigger the attack action
+            }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Attack"); // Reset the attack trigger after the boss attacks
+    }
+}
