07ecb9af3b6594758e33ac74cbd56b8aa5e50a51
LâmLmao <thanhlam03082004@gmail.com>
2024-10-19 22:47:15 +0700

    trap lỏ

--- Assets/SawTrap.cs
+++ Assets/SawTrap.cs
@@ -1,22 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SawTrap : MonoBehaviour
 {
     public int damage = 10; // Damage dealt by the trap
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Something entered the trap's trigger.");
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Player entered the trap's trigger.");
             PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
                 playerHealth.TakeDamage(damage);
                 Debug.Log("Player took " + damage + " damage from the saw trap!");
             }
-            Debug.Log("Player took " + damage + " damage from the saw trap!");
+            else
+            {
+                Debug.Log("PlayerHealth component not found on the player.");
+            }
         }
     }
 }
